ff942ff53eaf618ed7821df00e16ebc46ce621c9
EverNorif <1320346985@qq.com>
2022-08-01 19:06:54 +0800

    backend test

---
+++ backend.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+# app的配置选项
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"
+
+
+@app.route('/', methods=['GET'])
+def queryAnime():
+    temp = {"1": 2}
+    return jsonify(temp)
+
+
+if __name__ == '__main__':
+    app.run()
