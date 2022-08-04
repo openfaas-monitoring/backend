0f9faad0b7ad6c754a2d2892743160ddb3450554
EverNorif <1320346985@qq.com>
2022-08-04 13:53:24 +0800

    Basic PromQL Query

--- Monitor/prometheus.py
+++ Monitor/prometheus.py
@@ -1,11 +1,29 @@
 import requests
+import json
 
 
 # 提供PromQL的统一查询接口，供其他指标监控类调用
 class PromQL:
-    def __init__(self):
-        pass
+    def __init__(self, host):
+        self.host = host
+        self.api = 'http://' + host + '/api/v1/query'
+
+    def query(self, query):
+        res = {
+            'status': "error",
+            'data': '',
+        }
+        response = requests.get(self.api, {'query': query})
+        if response.status_code == 200:
+            response_json = json.loads(response.text)
+            res['status'] = response_json['status']
+            res['data'] = response_json['data']['result']
+        else:
+            print("请求错误,错误代码为" + str(response.status_code))
+
+        return res
 
 
 if __name__ == '__main__':
-    print("")
+    promQL = PromQL('10.60.150.24:31119')
+    promQL.query('http_requests_total{}[5m]')
