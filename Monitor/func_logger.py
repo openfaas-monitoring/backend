c76abe0e6183f492313d068e90a4f03984dae0e8
EverNorif <1320346985@qq.com>
2022-08-12 18:58:14 +0800

    add getLogsFromFunc

--- Monitor/func_logger.py
+++ Monitor/func_logger.py
@@ -1,4 +1,28 @@
+from kubernetes import client, config
+
+
+# 通过连接kubernetes来获取日志信息
+class FuncLogger:
+    def __init__(self, config_path):
+        self.k8s_client = config.new_client_from_config(config_path)
+        self.api = client.CoreV1Api(api_client=self.k8s_client)
+        self.func_map = self.getFuncAndPod()
+
+    def getFuncAndPod(self):
+        pods = self.api.list_namespaced_pod(namespace='openfaas-fn').items
+        func_map = dict()
+        for pod in pods:
+            pod_name = pod.metadata.name
+            func_name = pod_name.split('-')[0]
+            func_map[func_name] = pod_name
+        return func_map
+
+    def getLogsFromFunc(self, func_name, since_seconds=10):
+        logs = self.api.read_namespaced_pod_log(name=self.func_map[func_name], namespace='openfaas-fn',
+                                                since_seconds=since_seconds)
+        return logs
 
 
 if __name__ == '__main__':
-    print("haha")
+    funcLogger = FuncLogger('../resource/kubeconfig.yml')
+    print(funcLogger.getLogsFromFunc('errlog'))
